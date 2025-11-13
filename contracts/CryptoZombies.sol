a6d24172226348e02d11c9b046518558d49087a0
Thomas <akadosthomas@gmail.com>
2025-11-13 11:56:44 +0100

    Upload contracts folder

---
+++ contracts/CryptoZombies.sol
@@ -0,0 +1,9 @@
+pragma solidity ^0.8.0;
+import "./zombieownership.sol";
+
+contract CryptoZombies is ZombieOwnership
+    {
+    function kill() public onlyOwner {
+        selfdestruct(owner());
+    }
+}
