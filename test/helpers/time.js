36d1cc09737157a1b4654219249ec6f5dbaf86a5
Rasalghulsslayer <akadosthomas@gmail.com>
2025-11-13 16:47:34 +0100

    Minimal updates

--- test/helpers/time.js
+++ test/helpers/time.js
@@ -1,19 +1,54 @@
-async function increase(duration) {
+// La fonction n'a plus besoin d'être "async"
+function increase(duration) {
+    const id = Date.now();
 
-    //first, let's increase time
-    await web3.currentProvider.sendAsync({
-        jsonrpc: "2.0",
-        method: "evm_increaseTime",
-        params: [duration], // there are 86400 seconds in a day
-        id: new Date().getTime()
-    }, () => {});
+    return new Promise((resolve, reject) => {
+        // 1. Envoyer la commande evm_increaseTime
+        web3.currentProvider.send(
+            {
+                jsonrpc: '2.0',
+                method: 'evm_increaseTime',
+                params: [duration],
+                id: id,
+            },
+            err1 => {
+                if (err1) return reject(err1);
 
-    //next, let's mine a new block
-    web3.currentProvider.send({
-        jsonrpc: '2.0',
-        method: 'evm_mine',
-        params: [],
-        id: new Date().getTime()
-    })
-
+                // 2. Envoyer la commande evm_mine pour forcer la création d'un bloc
+                web3.currentProvider.send(
+                    {
+                        jsonrpc: '2.0',
+                        method: 'evm_mine',
+                        params: [],
+                        id: id + 1,
+                    },
+                    (err2, res) => {
+                        // Résoudre la Promise seulement après le minage
+                        return err2 ? reject(err2) : resolve(res);
+                    }
+                );
+            }
+        );
+    });
 }
+
+const duration = {
+    // ... (Le reste de l'objet duration reste inchangé)
+    seconds: function (val) {
+        return val;
+    },
+    minutes: function (val) {
+        return val * this.seconds(60);
+    },
+    hours: function (val) {
+        return val * this.minutes(60);
+    },
+    days: function (val) {
+        return val * this.hours(24);
+    },
+}
+
+module.exports = {
+    increase,
+    duration,
+};
